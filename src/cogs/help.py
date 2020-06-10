738ea6bb76aa1048f80c82df0d5d6352d13d972c
lukesaltweather <lukesaltweather@gmail.com>
2020-06-10 23:00:43 +0200

    help redesign

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -3,8 +3,19 @@ import itertools
 
 import discord
 from discord.ext import commands
+from discord.ext import menus
+from typing import List
+from discord.ext.commands.core import Group, Command
 
 from src.util import exceptions
+
+class Source(menus.ListPageSource):
+    def __init__(self, embeds: List[discord.Embed]):
+        super().__init__(embeds, per_page=1)
+
+    async def format_page(self, menu, page):
+        return page
+
 
 
 class MyHelpCommand(commands.MinimalHelpCommand):
@@ -17,10 +28,70 @@ class MyHelpCommand(commands.MinimalHelpCommand):
         self.aliases_heading = options.pop('aliases_heading', "Aliases:")
         self.no_category = options.pop('no_category', 'No Category')
         self.paginator = options.pop('paginator', None)
+        super().__init__(**options)
+
+    async def prepare_help_command(self, ctx, command):
         self.helper = EmbedHelper()
 
+    async def command_callback(self, ctx, *, command=None):
+        """|coro|
 
-        super().__init__(**options)
+        The actual implementation of the help command.
+
+        It is not recommended to override this method and instead change
+        the behaviour through the methods that actually get dispatched.
+
+        - :meth:`send_bot_help`
+        - :meth:`send_cog_help`
+        - :meth:`send_group_help`
+        - :meth:`send_command_help`
+        - :meth:`get_destination`
+        - :meth:`command_not_found`
+        - :meth:`subcommand_not_found`
+        - :meth:`send_error_message`
+        - :meth:`on_help_command_error`
+        - :meth:`prepare_help_command`
+        """
+        await self.prepare_help_command(ctx, command)
+        bot = ctx.bot
+
+        if command is None:
+            mapping = self.get_bot_mapping()
+            return await self.send_bot_help(mapping)
+
+        # Check if it's a cog
+        cog = bot.get_cog_insensitive(command)
+        if cog is not None:
+            return await self.send_cog_help(cog)
+
+        maybe_coro = discord.utils.maybe_coroutine
+
+        # If it's not a cog then it's a command.
+        # Since we want to have detailed errors when someone
+        # passes an invalid subcommand, we need to walk through
+        # the command group chain ourselves.
+        keys = command.split(' ')
+        cmd = bot.all_commands.get(keys[0])
+        if cmd is None:
+            string = await maybe_coro(self.command_not_found, self.remove_mentions(keys[0]))
+            return await self.send_error_message(string)
+
+        for key in keys[1:]:
+            try:
+                found = cmd.all_commands.get(key)
+            except AttributeError:
+                string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                return await self.send_error_message(string)
+            else:
+                if found is None:
+                    string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                    return await self.send_error_message(string)
+                cmd = found
+
+        if isinstance(cmd, Group):
+            return await self.send_group_help(cmd)
+        else:
+            return await self.send_command_help(cmd)
 
     def get_command_signature(self, command):
         return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self, command)
@@ -28,14 +99,6 @@ class MyHelpCommand(commands.MinimalHelpCommand):
     async def send_bot_help(self, mapping):
         ctx = self.context
         bot = ctx.bot
-        self.helper.reset()
-
-        if bot.description:
-            self.paginator.add_line(bot.description, empty=True)
-
-        note = self.get_opening_note()
-        if note:
-            self.paginator.add_line(note, empty=True)
 
         no_category = '\u200b{0.no_category}'.format(self)
         def get_category(command, *, no_category=no_category):
@@ -48,28 +111,26 @@ class MyHelpCommand(commands.MinimalHelpCommand):
         for category, commands in to_iterate:
             commands = sorted(commands, key=lambda c: c.name) if self.sort_commands else list(commands)
             if category != "MyCog":
+                self.helper.start_page(commands, category)
                 self.helper.add_cog(commands, category)
 
-        note = self.get_ending_note()
-        if note:
-            self.paginator.add_line()
-            self.paginator.add_line(note)
-
-        await self.context.message.add_reaction("📧")
-        await self.send_pages(cat="")
+        await self.send_pages()
+        self.helper.reset()
 
     async def send_command_help(self, command):
         self.helper.clear()
         self.helper.add_command(command)
-        await self.send_pages(cat="command")
+        await self.get_destination().send(embed=self.helper.embed[0])
 
-    async def send_pages(self, cat):
-        if cat == "command":
-            destination = self.context
-        else:
-            destination = self.context.author
-        for embed in self.helper.allembeds():
-            await destination.send(embed=embed)
+    async def send_cog_help(self, cog: commands.Cog):
+        cogcommands = cog.get_commands()
+        filtered = await self.filter_commands(cogcommands, sort=True)
+        self.helper.add_cog(filtered, cog.qualified_name)
+        await self.get_destination().send(embed=self.helper.embed[1])
+
+    async def send_pages(self):
+        menu = HelpMenu(source=Source(self.helper.allembeds()), delete_message_after=True, timeout=60)
+        await menu.start(self.context)
 
 
 
@@ -95,18 +156,77 @@ class MyCog(commands.Cog):
     def cog_unload(self):
         self.bot.help_command = self._original_help_command
 
+class HelpMenu(menus.MenuPages):
+    @menus.button('🔢', position=menus.Last(1))
+    async def select_page(self, payload):
+        channel = self.bot.get_channel(payload.channel_id)
+        i = await channel.send("Please enter the page you want to skip to:", delete_after=30)
+        def check(msg):
+            return msg.channel.id == payload.channel_id and msg.author.id == self.ctx.author.id
+        try:
+            message = await self.bot.wait_for('message', check=check, timeout=30)
+        except Exception:
+            pass
+        await message.delete()
+        await i.delete()
+        try:
+            msg_int = int(message.content)
+            await self.show_checked_page(msg_int-1)
+        except Exception:
+            await self.ctx.send("Please enter a valid number...", delete_after=30)
+
+    async def show_checked_page(self, page_number):
+        max_pages = self._source.get_max_pages()
+        try:
+            if max_pages is None:
+                # If it doesn't give maximum pages, it cannot be checked
+                await self.show_page(page_number)
+            elif max_pages > page_number >= 0:
+                await self.show_page(page_number)
+        except IndexError:
+            # An error happened that can be handled, so ignore it.
+            await self.ctx.send("This page doesn't exist...", delete_after=30)
+
 class EmbedHelper:
     """Create an Embed for the help command"""
     def __init__(self):
-        self.embed = [discord.Embed(color=discord.Colour.dark_green())]
-        self.embed[0].set_author(name="Help", icon_url="https://rei.animecharactersdatabase.com/uploads/chars/9225-1377974027.png")
-        self.embed[0].title = "Akashi Help"
-        self.embed[0].url = "https://docs.lukesaltweather.de"
-        self.embed[0].description = "Bot created by lukesaltweather@Nekyou.\nIf this help command doesn't help you with a problem, try visiting the docs first.\n" \
-                                    "All commands are described in detail on there, as well as how to write commands in general."
-        self.embed[0].set_footer(text="© Created by lukesaltweather#1111", icon_url="https://cdn.discordapp.com/avatars/358244935041810443/0c7effd92795854ef836c9ebe6404ff2.webp?size=1024")
+        self.embed = list()
+        self.page=1
+        self.cog_counter = 2
+
+    def start_page(self, commands, cog):
+        if len(self.embed) == 0:
+            self.embed.append(discord.Embed(color=discord.Colour.dark_blue()))
+            self.embed[0].set_author(name="Help", icon_url="https://rei.animecharactersdatabase.com/uploads/chars/9225-1377974027.png")
+            self.embed[0].title = "Akashi Help"
+            self.embed[0].url = "https://docs.lukesaltweather.de"
+            self.embed[0].set_footer(
+                icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Info_icon-72a7cf.svg/1200px-Info_icon-72a7cf.svg.png',
+                text="For more detailed help on a command, use $help <command>")
+            string = f"\n\n**{cog}**  |  Page: **{self.page+1}**\n"
+            first = True
+            for command in commands:
+                if first:
+                    string = f"{string} **`{command.name}`**"
+                else:
+                    string = f"{string} | **`{command.name}`**"
+
+            self.embed[0].description = f"{string}"
+        else:
+            string = f"\n\n**{cog}**  |  Page: **{self.page+1}**\n"
+            first = True
+            for command in commands:
+                if first:
+                    string = f"{string} **`{command.name}`**"
+                else:
+                    string = f"{string} | **`{command.name}`**"
+
+            self.embed[0].description = f"{self.embed[0].description}{string}"
+        self.page = self.page+1
+
+
     def add_cog(self, coms, cogname: str):
-        self.embed.append(discord.Embed(color=discord.Colour.dark_green()))
+        self.embed.append(discord.Embed(color=discord.Colour.dark_blue()))
         emoji = {
             "Info": "🔎",
             "Add": "📥",
@@ -114,7 +234,10 @@ class EmbedHelper:
             "Edit": "📝",
             "Assign": "🙋",
             "Done": "👍",
-            "Note": "🗒️"
+            "Note": "🗒️",
+            "MangaDex": "📖",
+            "ReminderCog": "📅",
+            "Tags": "📓"
         }.pop(cogname, "")
         self.embed[-1].set_author(name=f"{emoji} {cogname}")
         string = ""
@@ -122,9 +245,11 @@ class EmbedHelper:
             description = command.brief if command.description != "" else "No description"
             string = f"{string}[**`{command.name}`**]({command.help})\n*{description}*\n\n"
         self.embed[-1].description = string
+        self.embed[-1].set_footer(icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Info_icon-72a7cf.svg/1200px-Info_icon-72a7cf.svg.png', text=f"Page {self.cog_counter} | For more detailed help on a command, use $help <command>")
+        self.cog_counter = self.cog_counter+1
 
     def add_command(self, command):
-        self.embed.append(discord.Embed(color=discord.Colour.dark_green()))
+        self.embed.append(discord.Embed(color=discord.Colour.dark_blue()))
         self.embed[-1].set_author(name=command.name, icon_url="https://rei.animecharactersdatabase.com/uploads/chars/9225-1377974027.png")
         self.embed[-1].title = "Docs"
         self.embed[-1].url = command.help
@@ -138,14 +263,14 @@ class EmbedHelper:
 
     def reset(self):
         self.embed = []
-        self.embed = [discord.Embed(color=discord.Colour.dark_green())]
-        self.embed[0].set_author(name="Help",
-                                 icon_url="https://rei.animecharactersdatabase.com/uploads/chars/9225-1377974027.png")
-        self.embed[0].title = "Akashi Help"
-        self.embed[
-            0].description = "Bot created by lukesaltweather@Nekyou.\nIf this help command doesn't help you with a problem, try visiting the docs first.\n" \
-                             "All commands are described in detail on there, as well as how to write commands in general."
-        self.embed[0].set_footer(text="© Created by lukesaltweather#1111", icon_url="https://cdn.discordapp.com/avatars/358244935041810443/0c7effd92795854ef836c9ebe6404ff2.webp?size=1024")
+        # self.embed = [discord.Embed(color=discord.Colour.dark_blue())]
+        # self.embed[0].set_author(name="Help",
+        #                          icon_url="https://rei.animecharactersdatabase.com/uploads/chars/9225-1377974027.png")
+        # self.embed[0].title = "Akashi Help"
+        # self.embed[
+        #     0].description = "Bot created by lukesaltweather@Nekyou.\nIf this help command doesn't help you with a problem, try visiting the docs first.\n" \
+        #                      "All commands are described in detail on there, as well as how to write commands in general."
+        # self.embed[0].set_footer(text="© Created by lukesaltweather#1111", icon_url="https://cdn.discordapp.com/avatars/358244935041810443/0c7effd92795854ef836c9ebe6404ff2.webp?size=1024")
 
     def clear(self):
         self.embed = []

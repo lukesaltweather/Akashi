a4f3aef5d6ce06d5da259b2ae4f0167ac0c12cc9
Andreas <lukesaltweather@gmail.com>
2021-09-25 15:23:25 +0200

    Add note cog and noteflags

---
+++ src/util/flags/noteflags.py
@@ -0,0 +1,11 @@
+from src.util.flags.flagutils import ChapterFlags
+
+
+class AddNoteFlags(ChapterFlags):
+    text: str
+
+class RemoveNoteFlags(ChapterFlags):
+    pass
+
+class PurgeNotesFlags(ChapterFlags):
+    pass
